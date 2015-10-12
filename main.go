d30779ad77617ab2dd4adbddff4515e53390f395
Adam Ryman <adamryman@gmail.com>
2015-10-12 00:48:02 -0700

    Add code

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"net/http"
+	"time"
+)
+
+var msg = make(chan []byte)
+
+func wsHandler(ws *websocket.Conn) {
+	for {
+		wsMsg := <-msg
+		ws.Write(wsMsg)
+	}
+}
+
+func buttonPressed(w http.ResponseWriter, r *http.Request) {
+	for {
+		time.Sleep(time.Second)
+		msg <- []byte("hello, world")
+	}
+}
+
+func main() {
+	http.HandleFunc("/button", buttonPressed)
+	http.Handle("/socket", websocket.Handler(wsHandler))
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
